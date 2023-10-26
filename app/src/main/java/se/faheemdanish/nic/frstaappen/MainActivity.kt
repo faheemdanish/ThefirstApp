a0f239723816bcfacd085e1cad6d85771fde648d
Faheem Danish <danish42005@gmail.com>
2023-10-26 14:19:38 +0200

    Testa push den torsdag 26/10

--- app/src/main/java/se/faheemdanish/nic/frstaappen/MainActivity.kt
+++ app/src/main/java/se/faheemdanish/nic/frstaappen/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             FörstaAppenTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Kalle")
+                    Greeting("Kalle.push")
                 }
             }
         }
